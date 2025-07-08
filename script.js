bac626e9c9c6939f79bd71c197fa6d141bfb221e
ritam413 <ritamm413@gmail.com>
2025-07-08 19:03:51 +0530

    first comiit

---
+++ script.js
@@ -0,0 +1,153 @@
+
+const taskInput = document.getElementById('taskInput')
+const addTaskBtn = document.getElementById('addTaskBtn')
+const taskList = document.getElementById('taskList')
+const completed = document.getElementById('Completed')
+let count = 1
+
+const task = []
+
+function addingInTextArea(text) {
+    const li = document.createElement('li')
+    const span = document.createElement('span')
+
+    span.textContent = text;
+
+    li.appendChild(span)
+    li.className = "flex justify-between items-center px-4 py-2 bg-slate-100 rounded-md shadow-sm"
+    taskList.appendChild(li)
+}
+function addTask(text) {
+    const li = document.createElement('li')
+
+    const checkBoxesOnGoing = document.createElement('input')
+    checkBoxesOnGoing.type = "checkbox"
+    checkBoxesOnGoing.className = "flex justify-end w-4 h-4 accent-indigo-600";
+    const checkBoxesCompleted = document.createElement('input')
+    checkBoxesCompleted.type = "checkbox"
+    checkBoxesCompleted.className = "flex justify-end w-4 h-4 accent-indigo-600";
+
+    const checkboxSpan = document.createElement('span')
+    checkboxSpan.className = 'flex flex-row gap-2'
+
+    checkboxSpan.appendChild(checkBoxesOnGoing)
+    checkboxSpan.appendChild(checkBoxesCompleted)
+
+    const span = document.createElement('span')
+    span.textContent = `${count}) ${text}`
+    span.className = "text-slate-800"
+
+    li.appendChild(span)
+    li.appendChild(checkboxSpan)
+    li.className = "flex justify-between items-center px-4 py-2 bg-slate-100 rounded-md shadow-sm"
+    taskList.appendChild(li)
+
+    task.push({
+        id: count,
+        text: text,
+        isOngoing: false,
+        isCompleted: false,
+        elements: {
+            li,
+            span,
+            checkBoxesOnGoing,
+            checkBoxesCompleted
+        }
+    })
+
+    taskInput.value = '';
+    count++;
+}
+addTaskBtn.onclick = function () {
+    let text = taskInput.value;
+    console.log(text)
+    if (text === "") return
+    addTask(text)
+}
+
+function updateTextAreaWithCompletedTask() {
+    const completedTask = []
+
+    task.forEach(task => {
+        if (task.elements.checkBoxesCompleted.checked) {
+            completedTask.push(task.elements)
+            console.log(task.elements)
+        }
+    })
+
+    // console.log("Completed tasks:", completedTask);
+    taskList.textContent = ''
+
+    for (const task of completedTask) {
+        const text = task.span.textContent
+        addingInTextArea(text)
+    }
+
+}//fucntion to log completed task inconsole log
+
+
+
+function updateTextAreaWithOngoingTask() {
+    const onGoingTask = []
+    task.forEach(task => {
+        if (task.elements.checkBoxesOnGoing.checked) {
+            onGoingTask.push(task.elements)
+        }
+    })
+
+    taskList.textContent = ""
+
+    for (const task of onGoingTask) {
+        const text = task.span.textContent
+        addingInTextArea(text)
+    }
+}
+
+function updateTextAreaWithAllTask() {
+    taskList.textContent = ''
+    for (const alltasks of task) {
+        const text = alltasks.elements.span.textContent
+
+        if (text.trim() === "") {
+            continue
+        }
+        const li = document.createElement('li')
+        const span = document.createElement('span')
+
+        span.textContent = text;
+
+
+        li.className = "flex justify-between items-center px-4 py-2 bg-slate-100 rounded-md shadow-sm"
+        taskList.appendChild(li)
+        const checkBoxesOnGoing = document.createElement('input')
+        checkBoxesOnGoing.type = "checkbox"
+        checkBoxesOnGoing.className = "flex justify-end w-4 h-4 accent-indigo-600";
+        const checkBoxesCompleted = document.createElement('input')
+        checkBoxesCompleted.type = "checkbox"
+        checkBoxesCompleted.className = "flex justify-end w-4 h-4 accent-indigo-600";
+
+        const checkboxSpan = document.createElement('span')
+        checkboxSpan.className = 'flex flex-row gap-2'
+
+        checkboxSpan.appendChild(checkBoxesOnGoing)
+        checkboxSpan.appendChild(checkBoxesCompleted)
+
+        li.appendChild(span)
+        li.appendChild(checkboxSpan)
+    }
+
+    task.push({
+        id: count,
+        text: text,
+        isOngoing: false,
+        isCompleted: false,
+        elements: {
+            li,
+            span,
+            checkBoxesOnGoing,
+            checkBoxesCompleted
+        }
+    })
+
+}
+
